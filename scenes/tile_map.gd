560f1695cdcf0a77dbc6a0a35485f088b7fdedf0
burrito-cat <nathankinhluan@gmail.com>
2026-08-16 18:03:05 +0200

    Added back basic rotations. Need kick tables

--- scenes/tile_map.gd
+++ scenes/tile_map.gd
@@ -1,21 +1,51 @@
 extends Node2D
-
-@onready var board_layer: TileMapLayer = $board
-@onready var active_layer: TileMapLayer = $active
-@onready var HUD: CanvasLayer = $HUD
-
+@onready var active_layer = $active
+@onready var board_layer = $board
 #tetrominoes
-var I := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)]
-var T := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
-var O := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
-var Z := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)]
-var S := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)]
-var L := [Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
-var J := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var i_0 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)]
+var i_90 := [Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3)]
+var i_180 := [Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2)]
+var i_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3)]
+var i := [i_0, i_90, i_180, i_270]
 
+var t_0 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var t_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var t_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var t_270 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
+var t := [t_0, t_90, t_180, t_270]
 
-var shapes := [I, T, O, Z, S, J, L]
-var bag1 := shapes.duplicate()
+var o_0 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var o_90 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var o_180 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var o_270 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var o := [o_0, o_90, o_180, o_270]
+
+var z_0 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)]
+var z_90 := [Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var z_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var z_270 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]
+var z := [z_0, z_90, z_180, z_270]
+
+var s_0 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var s_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
+var s_180 := [Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2)]
+var s_270 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
+var s := [s_0, s_90, s_180, s_270]
+
+var l_0 := [Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var l_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var l_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2)]
+var l_270 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2)]
+var l := [l_0, l_90, l_180, l_270]
+
+var j_0 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var j_90 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(1, 2)]
+var j_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
+var j_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]
+var j := [j_0, j_90, j_180, j_270]
+
+var shapes := [i, t, o, z, s, l, j]
+var shapes_full := shapes.duplicate()
 
 #grid variables
 const COLS : int = 10
@@ -25,7 +55,7 @@ const ROWS : int = 20
 const directions := [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.DOWN]
 var steps : Array
 const steps_req : int = 50
-const start_pos := Vector2i(4, 0)
+const start_pos := Vector2i(5, 1)
 var cur_pos : Vector2i
 var speed : float
 const ACCEL : float = 0.25
@@ -49,10 +79,9 @@ var next_piece_atlas : Vector2i
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready(seed=1234):
-	seed(1234)
+func _ready():
 	new_game()
-	HUD.get_node("StartButton").pressed.connect(new_game)
+	$HUD.get_node("StartButton").pressed.connect(new_game)
 
 func new_game():
 	#reset variables
@@ -60,28 +89,30 @@ func new_game():
 	speed = 1.0
 	game_running = true
 	steps = [0, 0, 0] #0:left, 1:right, 2:down
-	HUD.get_node("GameOverLabel").hide()
+	$HUD.get_node("GameOverLabel").hide()
 	#clear everything
 	clear_piece()
 	clear_board()
 	clear_panel()
 	piece_type = pick_piece()
-	piece_atlas = Vector2i(bag1.find(piece_type), 0)
+	piece_atlas = Vector2i(shapes_full.find(piece_type), 0)
 	next_piece_type = pick_piece()
-	next_piece_atlas = Vector2i(bag1.find(next_piece_type), 0)
+	next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
 	create_piece()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# HAVE TO MAKE A NEW CONTROLLER SCRIPT THAT DEALS WITH INPUTS // je m'en occupe comme je l'ai fait avant
 	if game_running:
-		if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("move_left"):
 			steps[0] += 10
-		elif Input.is_action_pressed("ui_right"):
+		elif Input.is_action_pressed("move_right"):
 			steps[1] += 10
-		elif Input.is_action_pressed("ui_down"):
+		elif Input.is_action_pressed("soft_drop"):
 			steps[2] += 10
-
+		elif Input.is_action_just_pressed("cw"):
+			rotate_cw()
+		elif Input.is_action_just_pressed("ccw"):
+			rotate_ccw()
 		
 		#apply downward movement every frame
 		steps[2] += speed
@@ -97,7 +128,7 @@ func pick_piece():
 		shapes.shuffle()
 		piece = shapes.pop_front()
 	else:
-		shapes = bag1.duplicate()
+		shapes = shapes_full.duplicate()
 		shapes.shuffle()
 		piece = shapes.pop_front()
 	return piece
@@ -106,19 +137,32 @@ func create_piece():
 	#reset variables
 	steps = [0, 0, 0] #0:left, 1:right, 2:down
 	cur_pos = start_pos
-	active_piece = piece_type
+	active_piece = piece_type[rotation_index]
 	draw_piece(active_piece, cur_pos, piece_atlas)
 	#show next piece
-	draw_piece(next_piece_type, Vector2i(15, 6), next_piece_atlas)
+	draw_piece(next_piece_type[0], Vector2i(15, 6), next_piece_atlas)
 
 func clear_piece():
 	for i in active_piece:
-		active_layer.erase_cell(cur_pos + i)
+		active_layer.erase_cell( cur_pos + i)
 
 func draw_piece(piece, pos, atlas):
 	for i in piece:
 		active_layer.set_cell(pos + i, tile_id, atlas)
 
+func rotate_cw():
+	if can_rotate():
+		clear_piece()
+		rotation_index = (rotation_index + 1) % 4
+		active_piece = piece_type[rotation_index]
+		draw_piece(active_piece, cur_pos, piece_atlas)
+
+func rotate_ccw():
+	if can_rotate():
+		clear_piece()
+		rotation_index = (rotation_index - 1) % 4
+		active_piece = piece_type[rotation_index]
+		draw_piece(active_piece, cur_pos, piece_atlas)
 
 
 func move_piece(dir):
@@ -133,7 +177,7 @@ func move_piece(dir):
 			piece_type = next_piece_type
 			piece_atlas = next_piece_atlas
 			next_piece_type = pick_piece()
-			next_piece_atlas = Vector2i(bag1.find(next_piece_type), 0)
+			next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
 			clear_panel()
 			create_piece()
 			check_game_over()
@@ -145,8 +189,16 @@ func can_move(dir):
 		if not is_free(i + cur_pos + dir):
 			cm = false
 	return cm
-
-
+func find_lowest_transform(dir):
+	while can_move(dir):
+		pass
+func can_rotate():
+	var cr = true
+	var temp_rotation_index = (rotation_index + 1) % 4
+	for i in piece_type[temp_rotation_index]:
+		if not is_free(i + cur_pos):
+			cr = false
+	return cr
 
 func is_free(pos):
 	return board_layer.get_cell_source_id(pos) == -1
@@ -178,7 +230,7 @@ func check_rows():
 		else:
 			row -= 1
 
-func shift_rows(row): #shift rows down on line clear
+func shift_rows(row):
 	var atlas
 	for i in range(row, 1, -1):
 		for j in range(COLS):
@@ -186,7 +238,7 @@ func shift_rows(row): #shift rows down on line clear
 			if atlas == Vector2i(-1, -1):
 				board_layer.erase_cell(Vector2i(j + 1, i))
 			else:
-				active_layer.set_cell(Vector2i(j + 1, i), tile_id, atlas)
+				board_layer.set_cell(Vector2i(j + 1, i), tile_id, atlas)
 
 func clear_board():
 	for i in range(ROWS):
